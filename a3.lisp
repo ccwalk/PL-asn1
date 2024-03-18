a970a993794b8daeae52ef8ccdbc620286f4f071
Callie Walker <ccwalker@loyola.edu>
2024-03-18 12:35:35 -0400

    added a func for 2b & tests

--- a3.lisp
+++ a3.lisp
@@ -18,6 +18,20 @@
 
 (defun 2a (list-r) (let ((sum 0) (list-f '())) (dotimes (u (- (length list-r) 1) list-f) (dotimes (x (- (length list-r) 1)) (setq sum (+ sum (* (nth x list-r) (exp (round(realpart(expo u x list-r)))))))) (push sum list-f))))
 
+;;;Test
+
+(defvar list-r '(1 2 3 4))
+ (let ((result (2a list-r)))
+  (format t "Result: ~a~%" result))
 ;;; Question 2b
 
  (defun expoinv (u x list) (let ((N (length list))) (let ((result (* #C(0.0 1.0) (* 2 pi (/ (* u x) N))))) result)))
+
+(defun 2b (list-c) (let ((sum 0) (list-r '())) (dotimes (x (length list-c) list-r) (let ((sum 0)) (dotimes (u (length list-c)) (setq sum (+ sum (* (nth x list-c) (exp (round(realpart(expoinv x u list-c)))))))) (push (/ sum (length list-c)) list-r)) list-r)))
+
+;;;Test
+
+(defvar list-c '(40 30 20 10))
+ (let ((result (2b list-c)))
+  (format t "Result: ~a~%" result))
+
