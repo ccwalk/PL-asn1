5bf81b727a02963168260b36c66e072c65e36fea
EMMA SMITH <edsmith1@loyola.edu>
2024-03-17 21:29:17 -0400

    expoinv

--- a3.lisp
+++ a3.lisp
@@ -12,10 +12,12 @@
 ;;; Question 1
 (defun leibniz (n) "Returns a list of n terms of the Leibniz formula sequence" (declare (number n)) (if (and (plusp n) (integerp n)) (let ((lst '())) (push '+ lst) (dotimes (i n lst) (push (* (expt (- 1) (+ i 2)) (/ 4 (+ (* 2 i) 1))) lst)) (reverse lst))))
 
-;;; Question 2
+;;; Question 2a
 
 (defun expo (u x list) (let ((N (length list))) (let ((result (* #C(0.0 1.0) (* -2 pi (/ (* u x) N))))) result)))
 
 (defun 2a (list-r) (let ((sum 0) (list-f '())) (dotimes (u (- (length list-r) 1) list-f) (dotimes (x (- (length list-r) 1)) (setq sum (+ sum (* (nth x list-r) (exp (round(realpart(expo u x list-r)))))))) (push sum list-f))))
 
+;;; Question 2b
 
+ (defun expoinv (u x list) (let ((N (length list))) (let ((result (* #C(0.0 1.0) (* 2 pi (/ (* u x) N))))) result)))
